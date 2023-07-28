ac3ce79ce62dd2ed49f30028940e50a1b2f623d4
lkw <gewonlee99@gmail.com>
2023-07-28 10:29:48 +0900

    camel

--- src/main/java/com/kapple/domain/SupplFileVO.java
+++ src/main/java/com/kapple/domain/SupplFileVO.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class Suppl_fileVO {
+public class SupplFileVO {
 	private String suppl_file_no;
 	private String suppl_no;
 	private String supple_name;
