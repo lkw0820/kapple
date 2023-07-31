83c69633825f0356a8d5d0d3061d702f66f89d18
lkw <gewonlee99@gmail.com>
2023-07-31 12:17:24 +0900

    supplierList ing

--- src/main/java/com/kapple/controller/ControllerKW.java
+++ src/main/java/com/kapple/controller/ControllerKW.java
@@ -2,7 +2,11 @@ package com.kapple.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kapple.service.ServiceKW;
 
@@ -10,9 +14,15 @@ import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("")
+//@RequestMapping("")
 public class ControllerKW {
 
-//	@Autowired
-//	private ServiceKW service;
+	@Autowired
+	private ServiceKW service;
+	
+	@PostMapping("/comparsionSupplier") 
+	public void comparsionSupplier(@RequestParam("compo_name") String compo_name, Model model) {
+		log.info("supplierList...........");
+		model.addAttribute("sList",service.supplierListService(compo_name));
+	}
 }
--- src/main/java/com/kapple/mapper/MapperKW.java
+++ src/main/java/com/kapple/mapper/MapperKW.java
@@ -3,10 +3,12 @@ package com.kapple.mapper;
 import java.util.Date;
 import java.util.List;
 
+import com.kapple.domain.SupplierDetailVO;
 import com.kapple.domain.SupplierVO;
 
 public interface MapperKW {
 
 	public Date test();
-	public List<SupplierVO> supplierList();
+	public List<SupplierVO> supplierList(String compo_name);
+	public SupplierDetailVO detail(String suppl_no);
 }
