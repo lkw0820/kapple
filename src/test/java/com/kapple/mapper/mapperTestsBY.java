ed76743cee5b6dea0b9837c4bef867a11c2996e1
BoyunC <cbyn0819@gmail.com>
2023-08-02 18:05:45 +0900

    08/02 부품별 공급사 등록 : 공급사 등록 트랜잭션 코드 추가

--- src/test/java/com/kapple/mapper/mapperTestsBY.java
+++ src/test/java/com/kapple/mapper/mapperTestsBY.java
@@ -30,52 +30,52 @@ public class mapperTestsBY {
 //		mapper.getComponentList().forEach(component -> log.info(component));
 //	}
 	
-//	@Test
-//	public void insertSupplierTest() {
-//		log.info("insertSupplierTest Test==========================");
-//		
-//		SupplierVO supplier = new SupplierVO();
-//		supplier.setSuppl_no("SPPL20001");
-//		supplier.setSuppl_name("공급사테스트");
-//		supplier.setCeo_name("테스터");
-//		supplier.setCategory("직영점");
-//		supplier.setPhone("1111-1111");
-//		
-//		mapper.insertSupplier(supplier);
-//		
-//		log.info(supplier);
-//		
-//	}
-	
 	@Test
-	public void insertProposalTest() {
-		log.info("insertProposalTest Test==========================");
+	public void insertSupplierTest() {
+		log.info("insertSupplierTest Test==========================");
 		
-		ProposalVO proposal = new ProposalVO();
-	    SupplierVO supplier = new SupplierVO();
-	    ComponentVO component = new ComponentVO(); 
-
-	    proposal.setProposal_no("PROP501");
-	    
-	    // 공급사 정보 설정
-	    supplier.setSuppl_no("SPPL10843"); // 실제 공급사의 고유 번호를 설정해야 함
-	    proposal.setSupplier(supplier);
-
-	    // 부품 정보 설정
-	    component.setCompo_no("CPNT10057"); // 실제 부품의 고유 번호를 설정해야 함
-	    proposal.setComponent(component);
-
-	    proposal.setPrice(1235L);
-	    proposal.setQuantity(1234L);
-	    proposal.setDefective_rate(0.123f);
-	    proposal.setQuality_grade("A");
-	    proposal.setProd_period("5");
-	    proposal.setProposal_date(new Date());
-
-	    mapper.insertProposal(proposal);
-	    log.info(proposal);
+		SupplierVO supplier = new SupplierVO();
+	
+		supplier.setSuppl_name("공급사테스트1");
+		supplier.setCeo_name("테스터1");
+		supplier.setCategory("직영점1");
+		supplier.setPhone("1111-11111");
+		
+		mapper.insertSupplier(supplier);
+		
+		log.info(supplier);
 		
 	}
+	
+//	@Test
+//	public void insertProposalTest() {
+//		log.info("insertProposalTest Test==========================");
+//		
+//		ProposalVO proposal = new ProposalVO();
+//	    SupplierVO supplier = new SupplierVO();
+//	    ComponentVO component = new ComponentVO(); 
+//
+//	    proposal.setProposal_no("PROP501");
+//	    
+//	    // 공급사 정보 설정
+//	    supplier.setSuppl_no("SPPL10843"); // 실제 공급사의 고유 번호를 설정
+//	    proposal.setSupplier(supplier);
+//
+//	    // 부품 정보 설정
+//	    component.setCompo_no("CPNT10057"); // 실제 부품의 고유 번호를 설정
+//	    proposal.setComponent(component);
+//
+//	    proposal.setPrice(1235L);
+//	    proposal.setQuantity(1234L);
+//	    proposal.setDefective_rate(0.123f);
+//	    proposal.setQuality_grade("A");
+//	    proposal.setProd_period("5");
+//	    proposal.setProposal_date(new Date());
+//
+//	    mapper.insertProposal(proposal);
+//	    log.info(proposal);
+//		
+//	}
 
 	
 }
