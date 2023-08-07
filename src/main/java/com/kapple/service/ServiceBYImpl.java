059bba7a039d2393fe26b8d127990a88817616a3
BoyunC <cbyn0819@gmail.com>
2023-08-07 11:09:53 +0900

    08/07 부품별 공급사 등록 : 기능 구현 완료

--- src/main/java/com/kapple/service/ServiceBYImpl.java
+++ src/main/java/com/kapple/service/ServiceBYImpl.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kapple.domain.BizCertificateVO;
 import com.kapple.domain.ComponentVO;
+import com.kapple.domain.SupplDraftVO;
+import com.kapple.domain.SupplFileVO;
+import com.kapple.domain.SupplLogoVO;
 import com.kapple.mapper.MapperBY;
 import com.kapple.request.addSupplierRequest;
 
@@ -26,20 +30,69 @@ public class ServiceBYImpl implements ServiceBY {
 		return mapper.getComponentList();
 	}
 
+	@Override
+	public List<SupplDraftVO> getSupplDraftList() {
+		
+		return mapper.getSupplDraftList();
+	}
+	
+	@Override
+	public List<BizCertificateVO> getSupplInfoWithAddress() {
+		
+		return mapper.getSupplInfoWithAddress();
+	}
+	
+	@Override
 	@Transactional
-    @Override
-	public void registerSupplierAndProposal(addSupplierRequest request) {
-		try {
-			mapper.insertSupplierWithSK(request.getSupplier()); // 첫 번째 INSERT
-            // 첫 번째 INSERT 성공 후 새로 생성된 데이터를 받아온다.
-            String newSupplierId = request.getSupplier().getSuppl_no();
-            log.info("supple_no : " + newSupplierId);
+    public void registerSupplierAndProposal(addSupplierRequest request) {
+        try {
+        	log.info("registerSupplierAndProposal service method called.");
+        	//첫번째 작업 : 공급사 등록
+            mapper.insertSupplierWithSK(request.getSupplier());
             
+            request.getProposal().getSupplier().setSuppl_no(request.getSupplier().getSuppl_no());
+            
+            //두번째 작업 : 공급사 제안 등록
+            mapper.insertProposalWithSK(request.getProposal());
+            
+            System.out.println(request.getLogo_uuid());
+            System.out.println(request.getFile_uuid());
+            
+            if(!request.getLogo_uuid().isEmpty()) {
+            	//세번째 작업: 공급사 로고 업데이트
+            	mapper.updateSupplNoInLogo(request.getSupplier().getSuppl_no(), request.getLogo_uuid());
+            }
+            
+            if(!request.getFile_uuid().isEmpty()) {
+	            //네번째 작업: 공급사 파일 업데이트
+	            mapper.updateSupplNoInFile(request.getSupplier().getSuppl_no(), request.getFile_uuid());
+            }
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Transaction failed.", e);
+        }
+    }
+
+	@Override
+	public void registerSupplDraft(SupplDraftVO request) {
+		try {
+        	log.info("registerSupplDraft service method called.");
+
+            mapper.insertDraft(request);
         } catch (Exception e) {
             throw new RuntimeException("Transaction failed.", e);
         }
 	}
 
-	
+	@Override
+	public void registerSupplLogo(SupplLogoVO request) {
+		mapper.insertSupplLogo(request);
+	}
+
+	@Override
+	public void registerSupplFile(SupplFileVO request) {
+		mapper.insertSupplFile(request);
+	}
+
 
 }
